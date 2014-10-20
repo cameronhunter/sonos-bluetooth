dcb8ae6ed14cb0586ff0883475285dc8b241acd7
Cameron Hunter <hello@cameronhunter.co.uk>
2014-10-20 00:47:51 -0700

    Update startup.sh

--- bluetoothradio/startup.sh
+++ bluetoothradio/startup.sh
@@ -16,6 +16,9 @@ cd "$( cd -P $DIR && pwd )"
 su $user -c 'pulseaudio --start'
 
 # Set up the bluetooth adapter to be as visible as possible.
+service bluetooth restart
+rfkill unblock all
+hciconfig hci0 up
 hciconfig hci0 piscan
 
 # Load a bluetooth agent to allow the pairing and connection, and to
