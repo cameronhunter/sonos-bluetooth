db6598505717882ce1c91692afe44418fc5a0890
Cameron Hunter <chunter@twitter.com>
2015-03-03 01:07:40 -0800

    Simplify and rework Dockerfile and scripts. Still not functional.

--- bluetoothradio/agent.sh
+++
@@ -1,3 +0,0 @@
-# Load a bluetooth agent to allow the pairing and connection, and to
-# Set pulseaudio up against the bluetooth connection.
-bluetooth-agent $(cat bluetoothPin)
--- lib/startup.sh
+++ lib/startup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root" 1>&2
-  exit 1
-fi
 
-user="pulse"
-#cd "$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$( readlink -f "$( dirname "$0" )")
 cd "$( cd -P $DIR && pwd )"
 
@@ -13,21 +7,24 @@ cd "$( cd -P $DIR && pwd )"
 #modprobe snd_bcm2835
 
 # Start pulseaudio
-su $user -c 'pulseaudio --start'
+su $SONOS_USER -c 'pulseaudio --start'
 
 # Set up the bluetooth adapter to be as visible as possible.
 service bluetooth restart
-rfkill unblock all
-lsusb
+
+hciconfig hci0 name $SONOS_NAME
+hciconfig hci0 class 0x280404
+
 hciconfig -a dev
+
 hciconfig hci0 up
 hciconfig hci0 piscan
 
 # Load a bluetooth agent to allow the pairing and connection, and to
 # Set pulseaudio up against the bluetooth connection.
-(./agent.sh) &
+bluetooth-agent 1234 &
 
-./connect.sh
+sh /sonos-bluetooth/connect.sh
 
 while:
 do
